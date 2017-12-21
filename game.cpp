37610741b0d1e79d2742b3f9e1a4d48384b9bafc
Arne Vlietinck <arne.vlietinck@gmail.com>
2017-12-21 17:59:12 +0100

    Game: changed order of gameOver
    First play the sound, then blink the leds 3 times.

--- game.cpp
+++ game.cpp
@@ -8,12 +8,10 @@
 #include "player.h"
 #include "Buzzer.h"
 
-static int GAMEOVER = 1;
-
-/**
-* @return Returns a boolean which tells if the led should blink again.
-* @note The boolean is in 30% of the situations true and in the other 70% false.
-*/
+/*
+ * @return Returns a boolean which tells if the led should blink again.
+ * @note The boolean is in 30% of the situations true and in the other 70% false.
+ */
 bool shouldBlinkAgain()
 {
     int randomval = rand() % 10;
@@ -28,36 +26,36 @@ bool shouldBlinkAgain()
 }
 
 /**
-*
-*/
+ *
+ */
 int shouldRepeat(int currentLed)
 {
     /* interaction == false to avoid the situation where an interaction was true from the shouldRepeat
      * of the previous iteration and shouldRepeat is again true, which would lead
      * to a third repetition
      */
-    if(interaction == false && shouldBlinkAgain() == true)
+    if(interaction==false && shouldBlinkAgain()==true)
     {
         interaction = true;
     }
     //Something should have been done but the player didn't do it
-    else if(interaction == true && action == false)
+    else if(interaction==true && action==false)
     {
-        gameOver();
-        game = GAMEOVER;
         ADPCMSound sound(Buzzer_bin,Buzzer_bin_len);
         Player::instance().play(sound);
+        gameOver();
+        game = GAMEOVER;
     }
     //Nothing should have been done and the player did something
-    else if(interaction == false && action == true)
+    else if(interaction==false && action==true)
     {
-        gameOver();
-        game = GAMEOVER;
         ADPCMSound sound(Buzzer_bin,Buzzer_bin_len);
         Player::instance().play(sound);
+        gameOver();
+        game = GAMEOVER;
     }
     //Something should have been done and the player did it
-    else if(interaction == true && action == true)
+    else if(interaction==true && action==true)
     {
         //Back to normal
         interaction = false;
