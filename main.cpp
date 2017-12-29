fdccb0cb4f46255f11988097722e896630beec6a
Arne Vlietinck <arne.vlietinck@gmail.com>
2017-12-29 23:50:22 +0100

    Main: typo's and cleaner code
    Fixed some typo’s.
    Changed onOffBlinking(1) to reInitialisation() for cleaner main().

--- main.cpp
+++ main.cpp
@@ -16,35 +16,35 @@
 using namespace miosix;
 
 /**
- * Boolean which representate the state of the game.
+ * Boolean which represents the state of the game.
  * If game==1, the current game is finished.
  * Elseif game==0, the current game is still going.
  */
 bool game;
 
 /**
- * Boolean which representate the need of a players' interaction.
+ * Boolean which represents the need of a players' interaction.
  * When interaction is true, the player must do something
  * (eg: press the user button) to avoid a game over.
  */
 bool interaction;
 
 /**
- * Boolean which representate the action of a player.
+ * Boolean which represents the action of a player.
  * When action is true, the player did an action.
  * When action is false, the player didn't do an action.
  */
 bool action;
 
 /**
- * Integer which representate the difficulty level of the game.
+ * Integer which represents the difficulty level of the game.
  * Higher integer means higher degree of difficulty.
  * It affects the time between the blinking leds.
  */
 int difficulty;
 
 /**
- * Integer which representate the level of the game.
+ * Integer which represents the level of the game.
  * There are several gameplays implemented depending on the game level.
  */
 int level;
@@ -71,10 +71,12 @@ void mainInitialisation()
  * Reinitialisation process of the main. This is used to reset the game without
  * a complete reset by using the computer.
  *
- * @post difficulty is set on 1.
+ * @post Difficulty is set on 1.
  * @post Interaction is set on false.
  * @post Action is set on false while protected by mutex.
- * @post Game is set on 0;
+ * @post Game is set on 0.
+ * @post onOffBlinking(1) is executed.
+ * @see onOffBlinking()
  */
 void reInitialisation(){
   difficulty = 1;
@@ -86,6 +88,7 @@ void reInitialisation(){
   }
   game = 0;
   level = 0;
+  onOffBlinking(1); //To emphasize the start of the game.
 }
 
 /*! \mainpage Blinking Reaction Game
@@ -119,7 +122,6 @@ int main()
     {
         if(game==GAMEOVER) {
             reInitialisation();
-            onOffBlinking(1); /// To emphasize the start of the game.
         }
         blinkingGame();
     }
